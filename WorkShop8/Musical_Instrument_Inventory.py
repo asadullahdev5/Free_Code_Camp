3af790a9335dddbc9170678df8fcba5deeb4cd01
Asad Ullah <asadullahdev5@gmail.com>
2026-07-30 18:39:43 +0500

    Musical-Instrument-Inventory Class Code Done

---
+++ WorkShop8/Musical_Instrument_Inventory.py
@@ -0,0 +1,9 @@
+# Musical-Instrument-Inventory
+
+class MusicalInstrument:
+    def __init__(self, name, instrument_type):
+        self.name = name
+        self.instrument_type = instrument_type
+
+instrument_1 = MusicalInstrument('Oboe', 'woodwind')
+instrument_2 = MusicalInstrument('Trumpet', 'brass')
